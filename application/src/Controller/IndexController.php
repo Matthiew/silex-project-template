041183eb9da812190356d756982b7b20c3db14a8
Ben Youngblood <ben@synapsestudios.com>
2013-11-12 08:03:25 -0700

    add basic index controller

---
+++ application/src/Controller/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class IndexController
+{
+    public function indexAction()
+    {
+        return new Response();
+    }
+}
